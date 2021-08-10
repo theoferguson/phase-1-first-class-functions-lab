85e2fdf75100e6c583476bae3f6bee1ce3950ab4
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-10 13:53:18 -0400

    commit tree 09201e03af48b91f950c730828aa4a7552cae872

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const firstTwo = drivers.slice(0, 2);
+    return firstTwo;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastTwo = drivers.slice(-2);
+    return lastTwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return fare => fare * multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, whichDrivers) {
+    if (whichDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    } else if (whichDrivers === returnLastTwoDrivers) {
+            return returnLastTwoDrivers(drivers);
+        }
+}
